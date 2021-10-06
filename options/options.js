afddc8130a52a872ece304f6a0e98c762bedfc11
Crowdbotics <team@crowdbotics.com>
2021-10-06 18:31:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "hydrive_31174",
+  url: "https://hydrive_31174.botics.co",
+  api: "https://hydrive_31174.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
